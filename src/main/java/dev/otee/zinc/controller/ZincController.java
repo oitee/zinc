b57ced1f3a060986f10bb3691947a3231538bfc9
oitee <oitee.codes@gmail.com>
2024-03-14 09:51:43 +0530

    Update controller method for insert route

--- src/main/java/dev/otee/zinc/controller/ZincController.java
+++ src/main/java/dev/otee/zinc/controller/ZincController.java
@@ -3,6 +3,8 @@ package dev.otee.zinc.controller;
 import dev.otee.zinc.ZincService;
 import dev.otee.zinc.model.Note;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +20,11 @@ public class ZincController {
         return response;
     }
     @PostMapping("/note")
-    public Note create(@RequestBody Note note){
-        return service.insert(note);
+    public ResponseEntity create(@RequestBody Note note){
+        Note response = service.insert(note);
+        if (response == null){
+            return new ResponseEntity<String>("Title or Tag Missing", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Note>(response, HttpStatus.CREATED);
     }
 }
